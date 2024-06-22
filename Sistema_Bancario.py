b96b951ab8e4c8cf9ee6b0468526ff36a4588d46
Vinicius Paiva <144559632+V1n1c1u5P4iva@users.noreply.github.com>
2024-06-22 15:39:01 -0300

    Add files via upload

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -13,38 +13,52 @@ LIMITE_SAQUE = 3
 numero_de_saques = 0
 
 while True:
+    
     opcao = int(input(menu))
     
     if opcao == 1:
+        
         valor = int(input("Digite o valor a ser depositado: "))
+        
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R${valor:.2f}\n"
             print(f"O saldo agora é: R${saldo:.2f}")
+            
         else:
             print("Valor digitado inválido")
+            
     elif opcao == 2:
+        
         valor = int(input("Digite o valor do saque: "))
+        
         if valor > saldo:
             print("Saldo insuficiente")
+            
         elif valor > limite_saque:
             print("Ultrapassou o limite permitido")
+            
         elif numero_de_saques >= LIMITE_SAQUE:
             print("O limite de saque diários já foi atingido")
+            
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R${valor:.2f}\n"
             numero_de_saques += 1
             print(f"O saldo agora é: R${saldo:.2f}")
+            
         else:
             print("Valor digitado inválido")
+            
     elif opcao == 3:
         print(" Extrato ".center(28, "#"))
         print(f"Não tivemos movimentações" if not extrato else extrato)
         print(f"Saldo:R${saldo:.2f}")
         print("".center(28, "#"))
+        
     elif opcao == 4:
         print("Agradecemos sua preferência")
         break
+    
     else:
         print("Opção inválida!")
